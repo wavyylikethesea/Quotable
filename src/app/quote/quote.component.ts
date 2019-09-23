9203beb55a34b1054ffbfb1aeb48d947fc9ec0ef
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-09-23 11:01:28 +0300

    Add quotes initial array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote[] = [
+    {id:1, name:'Wastin air', details:'By Nipsey Hussle'},
+    {id:2, name:'Greatest human act is to inspire', details:'By Nipsey Hussle'},
+  ]
 
   constructor() { }
 
