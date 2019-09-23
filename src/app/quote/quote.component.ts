764d4dd628ed0b8cb2ae681dedb586d678535251
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-09-23 12:17:50 +0300

    Increase the number of initial quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,8 +9,11 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'Find your purpose or you wasting air', '- Nipsey Hussle'),
+    new Quote(1, 'The only true wisdom is in knowing you know nothing.', '- Socrates'),
     new Quote(2,'The greatest human act is to inspire', '- Nipsey Hussle'),
+    new Quote(3,'But man is not made for defeat. A man can be destroyed but not defeated.', '- Ernest Hemingway'),
+    new Quote(4,'When you reach the end of your rope, tie a knot in it and hang on.', '- Franklin D. Roosevelt'),
+    new Quote(5,'The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.', '- Hellen Keller'),
 
   ];
 
