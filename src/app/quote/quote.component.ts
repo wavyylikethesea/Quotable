5461219994c56e5a4428f1b807bf29e74878b3e4
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-09-23 11:06:18 +0300

    Edit initial quotes array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,7 +9,7 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    {id:1, name:'Wastin air', description:'By Nipsey Hussle'},
+    {id:1, name:'Find your purpose or you wastin air', description:'By Nipsey Hussle'},
     {id:2, name:'Greatest human act is to inspire', description:'By Nipsey Hussle'},
   ]
 
